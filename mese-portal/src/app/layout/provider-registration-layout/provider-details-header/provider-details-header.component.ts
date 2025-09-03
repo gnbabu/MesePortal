f9df1a5e43c245e30aa9b3703f3f6e2703763eaa
gnbabu <naresh.net2009@gmail.com>
2025-09-03 16:44:22 +0530

    Creating new components
    Creating Header component
    Creating horizontal nav bar component for provider registration

---
+++ mese-portal/src/app/layout/provider-registration-layout/provider-details-header/provider-details-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-provider-details-header',
+  imports: [],
+  templateUrl: './provider-details-header.component.html',
+  styleUrl: './provider-details-header.component.scss'
+})
+export class ProviderDetailsHeaderComponent {
+
+}
---
+++ mese-portal/src/app/layout/provider-registration-layout/provider-registration-navigation/provider-registration-navigation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-provider-registration-navigation',
+  imports: [],
+  templateUrl: './provider-registration-navigation.component.html',
+  styleUrl: './provider-registration-navigation.component.scss'
+})
+export class ProviderRegistrationNavigationComponent {
+
+}
